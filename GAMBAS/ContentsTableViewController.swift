ba936ac7c117f3b848befa262e173f652071be07
Songhee Choi <Songhee Choi>
2020-09-22 22:51:06 +0900

    콘텐츠뷰, 댓글 불러오기, 좋아요 기능 완료했지만, 좋아요 퍼포먼스 개선 필요, 자기가 쓴 댓글 삭제 아직 못함

--- GAMBAS/ContentsTableViewController.swift
+++ GAMBAS/ContentsTableViewController.swift
@@ -40,7 +40,6 @@ class ContentsTableViewController: UITableViewController, ContentsListQueryModel
     // table 뷰에서 함수 이름으로 데이터를 넘겨 줄 것임. 여기서 함수 이름 정의 먼저하고 -> table뷰로 이동!
     func subsInfoReceiveItems(_ prdSeqno:String){
          receive_prdSeqno = prdSeqno
-       
     }
     
     
@@ -131,7 +130,7 @@ class ContentsTableViewController: UITableViewController, ContentsListQueryModel
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
         
-        if segue.identifier == "sgContentsList"{
+        if segue.identifier == "sgDetailContents"{
             let cell = sender as! UITableViewCell
             let indexPath = self.contentsListTableView.indexPath(for: cell) // 몇 번쨰 클릭?
 
@@ -139,11 +138,12 @@ class ContentsTableViewController: UITableViewController, ContentsListQueryModel
             let item: SubscribeDBModel = contentsListFeedItem[(indexPath! as NSIndexPath).row] as! SubscribeDBModel // 받은 내용 몇번째인지 확인하고 DBModel로 변환한 후
             
             // 필요한정보 빼와서
-            let prdSeqno = String(item.ctSeqno!)
+            let ctSeqno = String(item.ctSeqno!)
+            let prdSeqno = String(item.prdSeqno!)
             
             
-            // ctSeqno 보내줌
-            // contentsListView.contentsInfoReceiveItems(ctSeqno) // 펑션만들기
+            // ctSeqno, prdSeqno 보내
+            contentsListView.contentsInfoReceiveItems(ctSeqno, prdSeqno: prdSeqno) // 펑션만들기
             
         }
         
--- SubscribeQueryModel.swift
+++ SubscribeQueryModel.swift
@@ -9,16 +9,27 @@
 import Foundation
 
 //프로토콜 ---
+// 구독 리스트 테이블
 protocol SubsListQueryModelProtocol: class{
     func subsListItemDownloaded(items:NSArray)
 }
-
+// 콘텐츠 리스트 테이블
 protocol ContentsListQueryModelProtocol: class{
     func contentsListItemDownloaded(items:NSArray)
+}
+// 콘텐츠 뷰
+protocol ContentsViewQueryModelProtocol: class{
+    func contentsViewItemDownloaded(items:NSArray)
+}
+
+// 댓글 리스트 테이블
+protocol CommentListQueryModelProtocol: class{
+    func commentListItemDownloaded(items:NSArray)
 }
 
 //---------
 
+// 구독리스트 테이블뷰
 class SubsListQueryModel: NSObject{
     
     var delegate: SubsListQueryModelProtocol!
@@ -118,7 +129,7 @@ class SubsListQueryModel: NSObject{
 }//----
 
 
-
+// 콘텐츠리스트 테이블뷰
 class ContentsListQueryModel: NSObject{
     
     var delegate: ContentsListQueryModelProtocol!
@@ -195,3 +206,162 @@ class ContentsListQueryModel: NSObject{
     }
     
 }//----
+
+
+// 콘텐츠뷰 get = ctSeqno
+class ContentsViewQueryModel: NSObject{
+    
+    var delegate: ContentsViewQueryModelProtocol!
+    var urlPath = "http://localhost:8080/gambas/contentsViewQuery.jsp"
+    
+    func contentsViewdownloadItems(ctSeqno: String?, uSeqno: String?){
+        print("받은ctSeqno", ctSeqno!, "받은 uSeqno", uSeqno!)
+        // urlPath 뒤에 ? 물음표 부터 뒤에 넣을 것 세팅
+        let urlAdd = "?ctSeqno=\(String(ctSeqno!))&uSeqno=\(String(uSeqno!))"
+        urlPath += urlAdd
+        
+        let url: URL = URL(string: urlPath)!
+        let defaultSession = Foundation.URLSession(configuration: URLSessionConfiguration.default)
+        
+        let task = defaultSession.dataTask(with: url){(data, response, error) in
+            if error != nil { // 에러코드가 없을 때 실행
+                print("Failed to download data")
+            }else{
+                print("ContentsView Data is downloaded")
+                
+                //parse JSON
+                self.parseJSON(data!)
+            }
+        }
+        task.resume()
+    }
+    
+    func parseJSON(_ data: Data){
+        var jsonResult = NSArray()
+           
+        do{
+            jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! NSArray
+        }catch let error as NSError{
+            print("contentsView parse error",error)
+        }
+            
+        var jsonElement = NSDictionary()
+        let locations = NSMutableArray()
+        
+        for i in 0..<jsonResult.count{
+            jsonElement = jsonResult[i] as! NSDictionary
+            let query = SubscribeDBModel()
+            
+            if  let ctSeqno = jsonElement["ctSeqno"] as? String,
+                let ctTitle = jsonElement["ctTitle"] as? String,
+                let ctContext = jsonElement["ctContext"] as? String,
+                let ctfile = jsonElement["ctfile"] as? String,
+                let ctRegistDate = jsonElement["ctRegistDate"] as? String,
+                let ctValidation = jsonElement["ctValidation"] as? String,
+                let prdSeqno = jsonElement["prdSeqno"] as? String,
+                let ctReleaseDate = jsonElement["ctReleaseDate"] as? String,
+                let countlikecontents = jsonElement["countlikecontents"] as? String,
+                let checkmylikecontents = jsonElement["checkmylikecontents"] as? String
+            
+            {
+                query.ctSeqno = ctSeqno
+                query.ctTitle = ctTitle
+                query.ctContext = ctContext
+                query.ctfile = ctfile
+                query.ctRegistDate = ctRegistDate
+                query.ctValidation = ctValidation
+                query.prdSeqno = prdSeqno
+                query.ctReleaseDate = ctReleaseDate
+                query.countlikecontents = countlikecontents
+                query.checkmylikecontents = checkmylikecontents
+            }
+            
+            // 배열에 넣어줌
+            locations.add(query)
+        }
+        
+        // 프로토콜로 전달
+        DispatchQueue.main.async(execute: {() -> Void in
+            self.delegate.contentsViewItemDownloaded(items: locations)
+        })
+        
+    }
+    
+}//----
+
+
+// 콘텐츠뷰 - 댓글 리스트 get = ctSeqno
+class CommentListQueryModel: NSObject{
+    
+    var delegate: CommentListQueryModelProtocol!
+    var urlPath = "http://localhost:8080/gambas/commentListQuery.jsp" //
+    
+    func commentListdownloadItems(ctSeqno: String?){
+        print("받은ctSeqno", ctSeqno!)
+        // urlPath 뒤에 ? 물음표 부터 뒤에 넣을 것 세팅
+        let urlAdd = "?ctSeqno=\(String(ctSeqno!))"
+        urlPath += urlAdd
+        
+        let url: URL = URL(string: urlPath)!
+        let defaultSession = Foundation.URLSession(configuration: URLSessionConfiguration.default)
+        
+        let task = defaultSession.dataTask(with: url){(data, response, error) in
+            if error != nil { // 에러코드가 없을 때 실행
+                print("Failed to download data")
+            }else{
+                print("CommentList Data is downloaded")
+                
+                //parse JSON
+                self.parseJSON(data!)
+            }
+        }
+        task.resume()
+    }
+    
+    func parseJSON(_ data: Data){
+        var jsonResult = NSArray()
+           
+        do{
+            jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! NSArray
+        }catch let error as NSError{
+            print("comment list parse error",error)
+        }
+            
+        var jsonElement = NSDictionary()
+        let locations = NSMutableArray()
+        
+        for i in 0..<jsonResult.count{
+            jsonElement = jsonResult[i] as! NSDictionary
+            let query = SubscribeDBModel()
+            
+            if  let cmSeqno = jsonElement["cmSeqno"] as? String,
+                let cmcontext = jsonElement["cmcontext"] as? String,
+                let cmRegistDate = jsonElement["cmRegistDate"] as? String,
+                let cmValidation = jsonElement["cmValidation"] as? String,
+                let ctSeqno = jsonElement["ctSeqno"] as? String,
+                let uSeqno = jsonElement["uSeqno"] as? String,
+                let uName = jsonElement["uName"] as? String
+            
+            {
+                query.cmSeqno = cmSeqno
+                query.cmcontext = cmcontext
+                query.cmRegistDate = cmRegistDate
+                query.cmValidation = cmValidation
+                query.ctSeqno = ctSeqno
+                query.uSeqno = uSeqno
+                query.uName = uName
+            }
+            
+            // 배열에 넣어줌
+            locations.add(query)
+        }
+        
+        // 프로토콜로 전달
+        DispatchQueue.main.async(execute: {() -> Void in
+            self.delegate.commentListItemDownloaded(items: locations)
+        })
+        
+    }
+    
+}//----
+
